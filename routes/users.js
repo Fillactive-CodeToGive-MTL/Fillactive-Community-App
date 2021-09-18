e7cab97af8a1acd6fdb82f089c55b508d477f75b
Pranav Kural <pranavkural@192.168.1.8>
2021-09-18 16:29:10 +0530

    temporary login to test user role

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.use('/userlogin', function(req, res, next) {
+  console.log("user login request received")
+  // update user_role
+  req.app.locals.user_role = req.body.user_role
+  res.redirect('/');
+});
+
 module.exports = router;
