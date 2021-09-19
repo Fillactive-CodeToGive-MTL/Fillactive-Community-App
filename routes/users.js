6066dabe6b26e579c5c325cbdfbd56e34800b9be
Pranav Kural <pranavkural@192.168.0.105>
2021-09-20 02:05:37 +0530

    added login basic functionality

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,23 @@ router.get('/', function(req, res, next) {
 
 router.use('/userlogin', function(req, res, next) {
   console.log("user login request received")
-  // update user_role
-  req.app.locals.user_role = req.body.user_role
-  res.redirect('/');
+  // get the username
+  let username = req.body.username
+  console.log(username)
+  console.log("**")
+  console.log(req.body)
+  // set user_role
+  let user_role = username == 'student@test.com' ? 'student'  : username == 'teacher@test.com' ? 'teacher' : ''
+  // if invalid username entered
+  if (user_role=='') {
+    req.app.locals.loginerror = true
+    req.app.locals.loginerrormessage = "Invalid username. Please try again!"
+    res.redirect('/')
+  } else {
+    // update user_role
+    req.app.locals.user_role = user_role
+    res.redirect('/feed');
+  }
 });
 
 module.exports = router;
